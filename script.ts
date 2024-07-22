9bedc97b9ae3209d4e34a59e889d9051740a9548
mohammed kaif <mkaif555123@gmail.com>
2024-07-22 20:35:27 +0500

    checking work

--- script.ts
+++ script.ts
@@ -1,2 +1,14 @@
-console.log("kaif");
-console.log(15 + 15);
+// console.log("kaif");
+// console.log(15 + 15);
+
+ const button =  document.getElementById("result") as HTMLButtonElement
+
+button.addEventListener("click", function (event) {
+    event.preventDefault();
+    let btnClick = document.getElementById("inp") as HTMLInputElement
+    let outputAns = document.getElementById("answer") as HTMLParagraphElement
+    outputAns.innerHTML = "";
+    for ( let i = 1; i <= 10; i++) {
+      outputAns.innerHTML += ` ${btnClick} + "x" + i + "=" + ${btnClick} * i + </br>`;
+    }
+  });
