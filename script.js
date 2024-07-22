ac43173d22441f41be612ff1fbe12524edc06beb
mohammed kaif <mkaif555123@gmail.com>
2024-07-22 20:51:47 +0500

    js file updated

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-// document.getElementById("result").addEventListener("click", function (event) {
-//     event.preventDefault();
-//     let btnClick = document.getElementById("inp").value;
-//     let outputAns = document.getElementById("answer");
-//     outputAns.innerHTML = "";
-//     for (i = 1; i <= 10; i++) {
-//       outputAns.innerHTML += btnClick + "x" + i + "=" + btnClick * i + "<br>";
-//     }
-//   });
+document.getElementById("result").addEventListener("click", function (event) {
+    event.preventDefault();
+    let btnClick = document.getElementById("inp").value;
+    let outputAns = document.getElementById("answer");
+    outputAns.innerHTML = "";
+    for (i = 1; i <= 10; i++) {
+      outputAns.innerHTML += btnClick + "x" + i + "=" + btnClick * i + "<br>";
+    }
+  });
